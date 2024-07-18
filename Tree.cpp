f82ae8ea0dad0d9871db95de981c51e638a80ba4
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-07-18 13:09:46 +0530

    Trees Lec-1 complete

--- Tree.cpp
+++ Tree.cpp
@@ -114,32 +114,72 @@ void postOrderTraversal(Node *root)
     cout << root->data << " ";
 }
 
+void buildFromLevelOrderTraversal(Node* &root)
+{
+    queue<Node *> q;
+
+    cout << "Enter data for root: " << endl;
+    int data;
+    cin >> data;
+
+    root = new Node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main()
 {
-    Node *root;
+    Node *root = NULL;
 
-    // creating a Tree
-    root = buildTree(root);
-    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-
-    // level order traversal
-    cout << "Printing the level order traversal" << endl;
+    buildFromLevelOrderTraversal(root);
     levelOrderTraversal(root);
-    cout<<endl;
+    // // creating a Tree
+    // root = buildTree(root);
+    // // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
 
-    // inOrder traversal
-    cout << "Printing the  inOrder traversal" << endl;
-    inOrderTraversal(root);
-    cout<<endl;
+    // // level order traversal
+    // cout << "Printing the level order traversal" << endl;
+    // levelOrderTraversal(root);
+    // cout << endl;
 
-    // preOrder traversal
-    cout << "Printing the  preOrder traversal" << endl;
-    preOrderTraversal(root);
-    cout<<endl;
+    // // inOrder traversal
+    // cout << "Printing the  inOrder traversal" << endl;
+    // inOrderTraversal(root);
+    // cout << endl;
 
-    // postOrder traversal
-    cout << "Printing the  postOrder traversal" << endl;
-    postOrderTraversal(root);
+    // // preOrder traversal
+    // cout << "Printing the  preOrder traversal" << endl;
+    // preOrderTraversal(root);
+    // cout << endl;
+
+    // // postOrder traversal
+    // cout << "Printing the  postOrder traversal" << endl;
+    // postOrderTraversal(root);
 
     return 0;
 }
