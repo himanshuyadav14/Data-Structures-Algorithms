5966756672cc3ea7b451b4383a229fb379ac70f4
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-07-19 15:52:40 +0530

    Done

--- Tree.cpp
+++ Tree.cpp
@@ -152,6 +152,39 @@ void buildFromLevelOrderTraversal(Node* &root)
     }
 }
 
+void BuildFromLevelOrderTraversal(Node* &root){
+    queue<Node*>q;
+
+    int data;
+    cin>> data;
+
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter the left node for: "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+
+        if(leftData != -1){
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter the right node for: "<<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+
+        if(rightData != -1){
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main()
 {
     Node *root = NULL;
