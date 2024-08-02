993edb03eb24ece7fd1b5f94afbc4ac90150675f
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-08-02 16:42:25 +0530

    Heap Sort done

--- Heaps/Heap.cpp
+++ Heaps/Heap.cpp
@@ -86,19 +86,36 @@ void heapify(int arr[], int size, int i)
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left < size && arr[largest] < arr[left])
+    if (left <= size && arr[largest] < arr[left])
     {
         largest = left;
     }
 
-    if (right < size && arr[largest] < arr[right])
+    if (right <= size && arr[largest] < arr[right])
     {
         largest = right;
     }
 
-    if(largest != i) {
+    if (largest != i)
+    {
         swap(arr[largest], arr[i]);
         heapify(arr, size, largest);
+    }
+}
+
+
+void heapSort(int arr[], int n){
+    int t = n;
+
+    while(t>1){
+        //Step1: Swap
+        swap(arr[1], arr[t]);
+        
+        //Step2: size--
+        t--;
+
+        //Step3: root ko correct pos
+        heapify(arr,t,1);
     }
 }
 
@@ -117,12 +134,24 @@ int main()
 
     int arr[6] = {-1, 54, 53, 55, 52, 50};
     int n = 5;
-    for(int i = n/2; i>0; i--){
-        heapify(arr,n,i);
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
     }
 
-    cout<<"Printing the array now"<<endl;
-    for(int i=1; i<=n; i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
+    cout << "Printing the array now" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    heapSort(arr,n);
+
+    cout << "Printing the array now" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
