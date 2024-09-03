6e837b65dcfa172cd4f80f056ce19dabb15a2f1e
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-09-03 08:59:24 +0530

    longest common prefix using trie

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -131,6 +131,28 @@ public:
     void removeWord(string word) {
         removeUtil(root, word);
     }
+
+    //LONGEST COMMON PREFIX UPDATED
+
+    // void lcp(string str, string& ans){
+    //     TrieNode* current = root;
+    //     for(int i=0; i<str.length(); i++){
+    //         char ch = str[i];
+
+    //         if(current->childCount == 1){
+    //             ans.push_back(ch);
+
+    //             // Move forward in the trie
+    //             int index = ch-'a';
+    //             current = current->children[index];
+    //         }else{
+    //             break;
+    //         }
+
+    //         if(current->isTerminal) break;
+    //     }
+    // }
+
 };
 
 int main()
