ba8d4c41b90d694f67cea1cffe9e2adfc1c41747
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-09-06 12:33:32 +0530

    Topological Sort using DFS and Kahn's Algo Implemented

--- Graph/TopologicalSort.cpp
+++ Graph/TopologicalSort.cpp
@@ -1,4 +1,5 @@
-// #include <bits/stdc++.h> 
+//Using BFS //Kahn's Algo
+
 // vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 //     unordered_map<int,vector<int>>adj;
 //     for(int i=0; i<e; i++){
@@ -34,6 +35,46 @@
 //         }
 //     }
 
+
+//     return ans;
+// }
+
+
+//Using DFS
+// void tsDFS(int node, unordered_map<int,vector<int>>& adj, unordered_map<int,bool>& visited, 
+// stack<int>&st){
+//     visited[node] = true;
+
+//     for(auto &neighbour:adj[node]){
+//         if(!visited[neighbour]){
+//             tsDFS(neighbour, adj, visited, st);
+//         }
+//     }
+
+//     st.push(node);
+// }
+// vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+//     unordered_map<int,vector<int>>adj;
+//     for(int i=0; i<e; i++){
+//         int u = edges[i][0];
+//         int w = edges[i][1];
+
+//         adj[u].push_back(w);
+//     }
+
+//     unordered_map<int,bool>visited;
+//     stack<int>st;
+//     for(int i=0; i<v; i++){
+//         if(!visited[i]){
+//             tsDFS(i, adj, visited, st);
+//         }
+//     }
+
+//     vector<int>ans;
+//     while(!st.empty()){
+//         ans.push_back(st.top());
+//         st.pop();
+//     }
 
 //     return ans;
 // }
