ec7688d84297bcb053dac96fe0c061360fe276b4
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-09-07 17:07:01 +0530

    critical connections by tarjan's algo done

--- Graph/Bridges.cpp
+++ Graph/Bridges.cpp
@@ -1,43 +1,48 @@
-// #include <vector>
-// using namespace std;
+// class Solution {
+// private:
+//     void DFS(int node, int parent, vector<vector<int>>& adj, vector<int>& disc,
+//     vector<int>& low, vector<bool>& visited, vector<vector<int>>& Bridges, int count){
+//         disc[node] = low[node] = count;
+//         visited[node] = true;
+//         count++;
 
-// void dfs(int u, vector<int>& disc, vector<int>& low, vector<int>& parent, vector<vector<int>>& adj, vector<vector<int>>& bridges, int& time) {
-//     disc[u] = low[u] = ++time;
-//     for (int v : adj[u]) {
-//         if (disc[v] == -1) {  // v is not visited
-//             parent[v] = u;
-//             dfs(v, disc, low, parent, adj, bridges, time);
+//         for(auto &nbr: adj[node]){
+//             if(nbr == parent) continue;
+//             else if(visited[nbr]){
+//                 low[node] = min(low[node], low[nbr]);
+//             }else{
+//                 DFS(nbr, node, adj, disc, low, visited, Bridges, count);
+//                 low[node] = min(low[node], low[nbr]);
 
-//             low[u] = min(low[u], low[v]);
-
-//             // If the lowest vertex reachable from subtree under v is below u in DFS tree, then u-v is a bridge
-//             if (low[v] > disc[u]) {
-//                 bridges.push_back({u, v});
+//                 //Bridge exist or not
+//                 if(low[nbr] > disc[node]){
+//                     Bridges.push_back({nbr,node});
+//                 }
 //             }
-//         } else if (v != parent[u]) {  // Back edge
-//             low[u] = min(low[u], disc[v]);
 //         }
+
 //     }
-// }
+// public:
+//     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+//         //creation of adjacency list;
+//         vector<vector<int>>adj(n);
+//         for(int i=0; i<connections.size(); i++){
+//             int u = connections[i][0];
+//             int v = connections[i][1];
 
-// vector<vector<int>> findBridges(vector<vector<int>> &edges, int v, int e) {
-//     vector<vector<int>> adj(v);
-//     for (const auto& edge : edges) {
-//         adj[edge[0]].push_back(edge[1]);
-//         adj[edge[1]].push_back(edge[0]);
-//     }
-
-//     vector<int> disc(v, -1);  // Discovery times of visited vertices
-//     vector<int> low(v, -1);   // Lowest points of visited vertices
-//     vector<int> parent(v, -1); // Parent vertices in DFS tree
-//     vector<vector<int>> bridges; // To store bridges
-//     int time = 0;
-
-//     for (int i = 0; i < v; i++) {
-//         if (disc[i] == -1) {
-//             dfs(i, disc, low, parent, adj, bridges, time);
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
 //         }
-//     }
 
-//     return bridges;
-// }
+//         vector<vector<int>>Bridges;
+//         vector<int>disc(n,0);
+//         vector<int>low(n+1,0);
+
+//         vector<bool>visited(n,false);
+//         int count = 0;
+
+//         DFS(0, -1, adj, disc, low, visited, Bridges, count);
+
+//         return Bridges;
+//     }
+// };
