0fd0e6b253be99394ecfa302599a18cc330b2cb4
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-09-09 12:09:16 +0530

    adjList done

--- Graph/adjList.cpp
+++ Graph/adjList.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-//space o(n)^2
+//space O(2*E)
 int main(){
     int n, m;
     cin>>n>>m;
+    vector<int>adj[n+1];
 
-    int adj[n+1][m+1];
-    
     for(int i=0; i<m; i++){
         int u, v;
         cin>>u>>v;
-        adj[u][v] = 1;
-        adj[v][u] = 1;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
     return 0;
 }
