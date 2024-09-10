815668b2097e5be30761ad29cb18ed010d6597fc
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-09-10 08:50:31 +0530

    dijkstra using pq

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -45,3 +45,50 @@
 
 //     return dist;
 // }
+
+
+//Priority queue
+// #include <bits/stdc++.h> 
+// vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
+//     //create adjacency List;
+//     unordered_map<int,vector<pair<int,int>>>adj;
+//     for(int i=0; i<edges; i++){
+//         int u = vec[i][0];
+//         int v = vec[i][1];
+//         int wt = vec[i][2];
+
+//         adj[u].push_back({v,wt});
+//         adj[v].push_back({u,wt});
+//     }
+
+//     //creation of distance array with infinite val;
+//     vector<int>dist(vertices);
+//     fill(dist.begin(),dist.end(),INT_MAX);
+
+//     //creation of set on basis of (distance, node)
+//     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+
+//     //Initialize distance and set with source node
+//     dist[source] = 0;
+//     pq.push({0,source});
+
+
+//     while(!pq.empty()){
+//         //Fetch top record
+//         auto top = pq.top();
+
+//         int nodeDistance = top.first;
+//         int topNode = top.second;
+
+//         pq.pop();
+        
+//         for(auto neighbour: adj[topNode]){
+//             if(dist[topNode] + neighbour.second < dist[neighbour.first]){
+//                 dist[neighbour.first] = dist[topNode] + neighbour.second;
+//                 pq.push({dist[neighbour.first],neighbour.first});
+//             }
+//         }
+//     }
+
+//     return dist;
+// }
