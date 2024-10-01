10745c8daa52faabbf4198c3ab43fe0a168e9585
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-10-01 10:01:56 +0530

    Heap sort

--- SortingAlgos/HeapSort.cpp
+++ SortingAlgos/HeapSort.cpp
@@ -43,42 +43,6 @@ public:
         }
         cout << endl;
     }
-
-    void deleteFromHeap()
-    {
-        if (size == 0)
-        {
-            cout << "Nothing to delete" << endl;
-            return;
-        }
-
-        // Step1: put last element into first index
-        arr[1] = arr[size];
-
-        // step2: Remove last element
-        size--;
-
-        // take root node to its correct position
-        int i = 1;
-        while (i < size)
-        {
-            int leftIndex = 2 * i;
-            int rightIndex = 2 * i + 1;
-
-            if (leftIndex < size && arr[i] < arr[leftIndex])
-            {
-                swap(arr[i], arr[leftIndex]);
-                i = leftIndex;
-            }
-            else if (rightIndex < size && arr[i] < arr[rightIndex])
-            {
-                swap(arr[i], arr[rightIndex]);
-                i = rightIndex;
-            }
-            else
-                return;
-        }
-    }
 };
 
 void heapify(int arr[], int size, int i)
@@ -130,7 +94,6 @@ int main()
     h.insert(54);
     h.print();
 
-    h.deleteFromHeap();
     h.print();
 
     int arr[6] = {-1, 54, 53, 55, 52, 50};
