8a0e37eb42ab2518c95cdf32f0ac035f3a21d5b5
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-10-01 08:00:48 +0530

    Bubble sort optimised

---
+++ SortingAlgos/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubbleSort(int* arr, int n){
+    for(int i=0; i<n-1; i++){
+        bool swapped = false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j+1] < arr[j]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            } 
+        }
+        if(!swapped) break;
+    }
+}
+
+int main(){
+    int arr[7] = {38,27,43,3,9,82,10};
+    int n = 7;
+    bubbleSort(arr, n);
+ 
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+}
