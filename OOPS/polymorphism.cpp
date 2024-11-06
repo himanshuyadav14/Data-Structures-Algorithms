b476f1e903df35a2f991076afd55c3f8f7166d6b
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-11-06 14:50:55 +0530

    polymorphism

--- OOPS/polymorphism.cpp
+++ OOPS/polymorphism.cpp
@@ -1,53 +1,95 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-// polymorphism
-class Human
-{
-public:
-    int height;
-    int weight;
-    int age;
 
-    int getAge()
-    {
-        return this->age;
+class A {
+
+    public:
+    void sayHello() {
+        cout << "Hello Love Babbar" << endl;
+    }
+    
+    int sayHello(char name) {
+        cout << "Hello Love Babbar" << endl;
+        return 1;
     }
 
-    void setWeight(int wt)
-    {
-        this->weight = wt;
-    }
-};
-
-// Inherit
-class Male : private Human
-{
-public:
-    string color;
-
-    void sleep()
-    {
-        cout << "Male is sleeping" << endl;
-    }
-
-    int getHeight(){
-        return this->height;
+    void sayHello(string name) {
+        cout << "Hello " << name  << endl;
     }
 };
 
-int main()
-{
-    Male object1;
-    cout<<object1.getHeight()<<endl;
+class B {
+    public:
+    int a;
+    int b;
 
-    // cout<<object1.age<<endl;
-    // cout<<object1.height<<endl;
-    // cout<<object1.weight<<endl;
-    // cout<<object1.color<<endl;
-    // object1.sleep();
+    public: 
+    int add() {
+        return a+b;
+    }
 
-    // object1.setWeight(30);
-    // cout<<object1.weight<<endl;
+    void operator+ (B &obj) {
+        int value1 = this -> a;
+        int value2 = obj.a;
+        cout << "output " << value2 - value1 << endl; 
+
+       cout << "Hello Babbar" << endl;
+    }
+
+    void operator() () {
+        cout << "main Bracket hu " << this->a << endl;
+    }
+
+};
+
+class Animal {
+    public:
+    void speak() {
+        cout << "Speaking "<< endl;
+    }
+};
+
+class Dog: public Animal {
+
+    public:
+    void speak() {
+        cout << "Barking " << endl;
+    }
+
+
+};
+
+
+
+int main() {
+
+    Dog obj;
+    obj.speak();
+
+
+
+
+/*
+    B obj1, obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+    obj1();*/
+
+
+
+
+
+
+
+
+/*
+    A obj;
+    obj.sayHello();
+    */
+
 
     return 0;
 }
